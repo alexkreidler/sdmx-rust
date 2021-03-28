7ef7270ab3f13ced2ddbf31e18fee4e76ea3f9bc
Alex Kreidler <alexkreidler2020@gmail.com>
2021-03-28 15:49:02 -0400

    refactor warc

--- lib/crawler.rs
+++ lib/crawler.rs
@@ -1,8 +1,9 @@
-use reqwest::Client;
+use http::HeaderMap;
+use reqwest::{Body, Client};
 use warc::{RawRecord, Record, RecordType, WarcWriter};
 
 use crate::queries::metadata_query;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use chrono::Utc;
 use std::{collections::HashMap, time::Instant};
 use std::{convert::TryFrom, string::ToString};
@@ -10,21 +11,50 @@ use std::{fs::OpenOptions, path};
 use ulid::Ulid;
 use url::Url;
 
-pub async fn write_response(res: reqwest::Response) -> Result<()> {
-    // Serialize response
-    let heads = res.headers();
-
+fn serialize_headers(headers: &HeaderMap) -> String {
     // Does this header serialization method support multi-line headers
-    let headers: Vec<String> = heads
+    let each: Vec<String> = headers
         .into_iter()
         .map(|(k, v)| vec![k.as_str(), v.to_str().unwrap()].join(": "))
         .collect();
 
-    let out_headers = headers.join("\r\n");
-    let response_serialized = res.text().await?;
-    println!("{:#?}", response_serialized);
-    let fin = vec![out_headers, response_serialized].join("\r\n\r\n");
-    let body = fin.as_bytes();
+    each.join("\r\n")
+}
+
+pub async fn write_request(req: reqwest::Request) -> anyhow::Result<()> {
+    let bd = req.body().ok_or(anyhow!("No body"))?;
+
+    bd.as_bytes();
+
+    Ok(())
+}
+
+pub async fn write_response(res: reqwest::Response) -> Result<()> {
+    // Serialize response
+    let headers = res.headers().clone();
+    // let response_serialized = res.text().await?;
+    // let bd = res.body().ok_or_else(|| Err(anyhow!("No body")))?;
+
+    let body = res.text().await?;
+    let bd = body.as_bytes();
+
+    write_warc(RecordType::Response, &headers, bd).await?;
+
+    Ok(())
+}
+
+pub async fn write_warc(
+    typ: RecordType,
+    headers: &HeaderMap,
+    body: &[u8],
+) -> Result<()> {
+    let out_headers = serialize_headers(headers);
+
+    // https://users.rust-lang.org/t/what-is-right-ways-to-concat-strings/3780/3
+    let fin = out_headers + "\r\n\r\n";
+    let before = fin.as_bytes();
+    // https://stackoverflow.com/questions/40792801/best-way-to-concatenate-vectors-in-rust
+    let warc_body = [before, body].concat();
 
     // Write WARC File
     let dir = "./warc-out";
@@ -41,20 +71,8 @@ pub async fn write_response(res: reqwest::Response) -> Result<()> {
     let mut record = Record::default();
     record.set_warc_version("1.1");
     record.set_warc_id(id);
-    record.set_warc_type(RecordType::Response);
-    record.replace_body(body);
-
-    // {
-    //     raw: RawRecord {
-    //         version: "WARC/1.1".to_string(),
-    //         headers: HashMap::new(),
-    //         body: vec![],
-    //     },
-    //     record_date: Utc::now(),
-    //     record_id: id,
-    //     record_type: RecordType::Response,
-    //     truncated_type: None,
-    // };
+    record.set_warc_type(typ);
+    record.replace_body(warc_body);
 
     writer.write(&record)?;
 
